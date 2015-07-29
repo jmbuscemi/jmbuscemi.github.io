1a056bd996a8fcff4a0242da0c3d85f7dd6490be
John M Buscemi <jmbuscemi@gmail.com>
2015-07-29 00:20:11 -0400

    Update styles.  Need to replace pictures

--- js/main.js
+++ js/main.js
@@ -18,10 +18,19 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top
+            scrollTop: target.offset().top
         }, 1000);
         return false;
       }
     }
   });
 });
+
+
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+
+try {
+  var pageTracker = _gat._getTracker("UA-65683527-1");
+pageTracker._trackPageview();
+} catch(err) {}
