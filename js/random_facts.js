dc3262371b816da99cf9a6eb605a57b119e4a572
John M Buscemi <jmbuscemi@gmail.com>
2015-08-27 11:37:11 -0400

    Add more facts

--- js/random_facts.js
+++ js/random_facts.js
@@ -1,12 +1,14 @@
 var randomFact = {
   "facts": [
-    {"text":"I'm detail oriented"},
+    {"text":"I've fed wild Wallabies in Australia"},
     {"text":"I've surfed in South Africa"},
     {"text":"I've cooked dinner for a Senator"},
     {"text":"I like chocolate chip cookies"},
     {"text":"I'm a pretty decent point guard"},
     {"text":"Go Wolfpack!!!"},
-    {"text":"I write and perform my own music"}
+    {"text":"I write and perform my own music"},
+    {"text":"I like math"},
+    {"text":"You might fly on a jet engine I helped develop"}
   ]
 };
 
@@ -14,13 +16,11 @@ function generateRandomFact(first, last) {
   return Math.floor(Math.random() * (last - first + 1)) + first;
 }
 
-randomFactNum = generateRandomFact(1, 7) - 1;
-// alert(randomFactNum);
-// alert(randomFact.facts[randomFactNum].text);
-$("#fact-text").append( randomFact.facts[randomFactNum].text );
+randomFactIndex = generateRandomFact(1, 9) - 1;
+$("#fact-text").append( randomFact.facts[randomFactIndex].text );
 
 function onClick() {
-  randomFactNum = generateRandomFact(1, 7) - 1;
+  randomFactIndex = generateRandomFact(1, 9) - 1;
   $( "#fact-text" ).empty();
-  $( "#fact-text" ).prepend( randomFact.facts[randomFactNum].text );
+  $( "#fact-text" ).prepend( randomFact.facts[randomFactIndex].text );
 }
