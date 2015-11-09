64ce4a63a19080d0bbe392bc77a4228d0922b83a
John M Buscemi <jmbuscemi@gmail.com>
2015-11-08 23:55:32 -0500

    Add scrollspy to highlight navbar based on location

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Scrolls to the selected menu item on the page
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
+    //Figure out code to remove $('.spy-active') to stop scrollspy on smooth scroll.
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
@@ -20,11 +21,13 @@ $(".nav li a").click(function(){
   $(this).parent().addClass("active");
 });
 
+//Change Navbar to Active on Scroll via scrollspy
+$('body').scrollspy({ target: '.spy-active' });
+
 // Close collapsed menu on click
 $(".nav li a").click(function(){
   $(".navbar-collapse").collapse('hide');
 });
-
 
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
