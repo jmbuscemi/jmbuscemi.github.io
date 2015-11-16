88d610c7d2abbc75b33b0809cb90d4063bdf2560
John M Buscemi <jmbuscemi@gmail.com>
2015-11-15 20:49:36 -0500

    Update google anayltics

--- js/main.js
+++ js/main.js
@@ -26,11 +26,3 @@ $(".nav li a").click(function(){
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
-
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-
-try {
-  var pageTracker = _gat._getTracker("UA-65683527-1");
-pageTracker._trackPageview();
-} catch(err) {}
